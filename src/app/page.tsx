fc049e3635ad1589ee6ea824b36b51bdc5345991
bradleyhung <bradleyhung5570@gmail.com>
2025-09-11 21:29:33 -0400

    Add Welcome component and integrate it into the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/header";
 import Topics from "./components/topics";
+import Welcome from "./components/welcome";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Topics />
+      <Welcome />
     </main>
   );
 }
